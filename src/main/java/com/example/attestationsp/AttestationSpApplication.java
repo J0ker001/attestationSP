8602f137192368113bbaf87c2a52064d31f7b247
Антон <antonL1790@yandex.ru>
2023-05-03 06:32:29 +0300

    Initial commit

---
+++ src/main/java/com/example/attestationsp/AttestationSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.attestationsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttestationSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttestationSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/attestationsp/AttestationSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attestationsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttestationSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
